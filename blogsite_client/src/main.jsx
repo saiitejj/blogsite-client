7f306f3635d4883c4bef2b8385059fd06fa404bd
Sai Tej <ngvst2004@gmail.com>
2025-10-16 02:16:42 +0530

    feat: Install axios and react-router and set up BrowserRouter

--- blogsite_client/src/main.jsx
+++ blogsite_client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 1. Import BrowserRouter
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* 2. Wrap the App component */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
